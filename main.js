23e1cd3e28423a8be196edd4ec572ab129dc8ec5
romain.casagrande.pro@gmail.com <romain.casagrande.pro@gmail.com>
2023-05-16 20:32:12 +0200

    add-html-pokeApi-and-js

--- main.js
+++ main.js
@@ -16,17 +16,4 @@ function addListe() {
   }
 }
 
-// fetch("https://pokeapi.co/api/v2/pokedex/2/")
-//   .then((reponse) => reponse.json())
-//   .then((data) => {
-//     pokemon = data.pokemon_entries;
-//     console.log(data);
-//     for (let i = 0; i < pokemon.length; i++) {
-//       const nbrPo = pokemon[i];
-//       console.log(nbrPo);
-//     }
-//     // for (let j = 0; j < entry.pokemon_species.name.length; j++) {
-//     //   const species = entry.pokemon_species.name[j];
-//     //   console.log(species);
-//     // }
-//   });
+
