3083519dec5d5209b231b55eb0983f5cbb9f0d22
romain.casagrande.pro@gmail.com <romain.casagrande.pro@gmail.com>
2023-05-16 21:12:59 +0200

    fetch-Api-pokemon

--- pokeApi.js
+++ pokeApi.js
@@ -1,14 +1,18 @@
+let section = document.getElementById(`pokemonInfo`)
+
+
+
 fetch("https://pokeapi.co/api/v2/pokedex/2/")
   .then((reponse) => reponse.json())
   .then((data) => {
     pokemon = data.pokemon_entries;
-    console.log(data);
+    console.log(data.pokemon_entries);
     for (let i = 0; i < pokemon.length; i++) {
-      const nbrPo = pokemon[i];
-      console.log(nbrPo);
-    }
-    // for (let j = 0; j < entry.pokemon_species.name.length; j++) {
-    //   const species = entry.pokemon_species.name[j];
-    //   console.log(species);
-    // }
+        const nbrPo = pokemon[i].entry_number;
+        const name = pokemon[i].pokemon_species.name;
+        const img =pokemon[i].pokemon_species.url
+        let cardPokemon = document.createElement("p");
+        cardPokemon.innerHTML=`NUMERO: ${nbrPo} , NOM : ${name} ,` ;
+        section.append(cardPokemon);
+    } 
   });
